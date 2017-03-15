326be4771938ff1022f4686e8fbeecf85581b9d5
rtdazure <rtdazure>
2017-03-15 12:53:11 +0100

    Step 4: Questions

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
     .state('main', {
       url: '/',
-      component: 'messages'
+      component: 'questions'
     });
     /*
     .state('technologies', {
